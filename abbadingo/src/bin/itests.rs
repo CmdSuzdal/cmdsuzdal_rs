925e5ec39f33ffc7b610216ac9362fe2f9e178e0
Francesco Piantini <francesco.piantini@gmail.com>
2021-10-13 22:31:04 +0200

    [abbadingo] BitBoard: playing with integration tests

--- abbadingo/src/bin/itests.rs
+++ abbadingo/src/bin/itests.rs
@@ -1,6 +1,11 @@
-use abbadingo::bitboard::BitBoard;
+use abbadingo::bitboard::{BitBoard, Rank, File, Cell};
 
 fn main() {
     let bb = BitBoard::new();
     println!("Hello Bitboard: {:?}", bb);
+
+    let f = File::FileA;
+    let r = Rank::Rank1;
+    let c = Cell::A1;
+    println!("File is {:?}, Rank is {:?}, Cell is {:?}", f, r, c);
 }
