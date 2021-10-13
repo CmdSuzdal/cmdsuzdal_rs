dded9d80d459520859f9ed741338aa1ae0632151
Francesco Piantini <francesco.piantini@gmail.com>
2021-10-13 23:26:56 +0200

    [abbadingo] bbdefines: added test section; added test for the to_cell() function

--- abbadingo/src/bbdefines.rs
+++ abbadingo/src/bbdefines.rs
@@ -97,3 +97,80 @@ pub fn to_cell(f: File, r: Rank) -> Cell {
     // and the result of the from_u32 is a valid Cell for sure
     num::FromPrimitive::from_u32((r as u32) * 8 + (f as u32)).unwrap()
 }
+
+// ****************************************************************************
+// TESTS
+// ****************************************************************************
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn try_all_to_cell_conversions() {
+        assert_eq!(to_cell(File::FileA, Rank::Rank1), Cell::A1);
+        assert_eq!(to_cell(File::FileA, Rank::Rank2), Cell::A2);
+        assert_eq!(to_cell(File::FileA, Rank::Rank3), Cell::A3);
+        assert_eq!(to_cell(File::FileA, Rank::Rank4), Cell::A4);
+        assert_eq!(to_cell(File::FileA, Rank::Rank5), Cell::A5);
+        assert_eq!(to_cell(File::FileA, Rank::Rank6), Cell::A6);
+        assert_eq!(to_cell(File::FileA, Rank::Rank7), Cell::A7);
+        assert_eq!(to_cell(File::FileA, Rank::Rank8), Cell::A8);
+        assert_eq!(to_cell(File::FileB, Rank::Rank1), Cell::B1);
+        assert_eq!(to_cell(File::FileB, Rank::Rank2), Cell::B2);
+        assert_eq!(to_cell(File::FileB, Rank::Rank3), Cell::B3);
+        assert_eq!(to_cell(File::FileB, Rank::Rank4), Cell::B4);
+        assert_eq!(to_cell(File::FileB, Rank::Rank5), Cell::B5);
+        assert_eq!(to_cell(File::FileB, Rank::Rank6), Cell::B6);
+        assert_eq!(to_cell(File::FileB, Rank::Rank7), Cell::B7);
+        assert_eq!(to_cell(File::FileB, Rank::Rank8), Cell::B8);
+        assert_eq!(to_cell(File::FileC, Rank::Rank1), Cell::C1);
+        assert_eq!(to_cell(File::FileC, Rank::Rank2), Cell::C2);
+        assert_eq!(to_cell(File::FileC, Rank::Rank3), Cell::C3);
+        assert_eq!(to_cell(File::FileC, Rank::Rank4), Cell::C4);
+        assert_eq!(to_cell(File::FileC, Rank::Rank5), Cell::C5);
+        assert_eq!(to_cell(File::FileC, Rank::Rank6), Cell::C6);
+        assert_eq!(to_cell(File::FileC, Rank::Rank7), Cell::C7);
+        assert_eq!(to_cell(File::FileC, Rank::Rank8), Cell::C8);
+        assert_eq!(to_cell(File::FileD, Rank::Rank1), Cell::D1);
+        assert_eq!(to_cell(File::FileD, Rank::Rank2), Cell::D2);
+        assert_eq!(to_cell(File::FileD, Rank::Rank3), Cell::D3);
+        assert_eq!(to_cell(File::FileD, Rank::Rank4), Cell::D4);
+        assert_eq!(to_cell(File::FileD, Rank::Rank5), Cell::D5);
+        assert_eq!(to_cell(File::FileD, Rank::Rank6), Cell::D6);
+        assert_eq!(to_cell(File::FileD, Rank::Rank7), Cell::D7);
+        assert_eq!(to_cell(File::FileD, Rank::Rank8), Cell::D8);
+        assert_eq!(to_cell(File::FileE, Rank::Rank1), Cell::E1);
+        assert_eq!(to_cell(File::FileE, Rank::Rank2), Cell::E2);
+        assert_eq!(to_cell(File::FileE, Rank::Rank3), Cell::E3);
+        assert_eq!(to_cell(File::FileE, Rank::Rank4), Cell::E4);
+        assert_eq!(to_cell(File::FileE, Rank::Rank5), Cell::E5);
+        assert_eq!(to_cell(File::FileE, Rank::Rank6), Cell::E6);
+        assert_eq!(to_cell(File::FileE, Rank::Rank7), Cell::E7);
+        assert_eq!(to_cell(File::FileE, Rank::Rank8), Cell::E8);
+        assert_eq!(to_cell(File::FileF, Rank::Rank1), Cell::F1);
+        assert_eq!(to_cell(File::FileF, Rank::Rank2), Cell::F2);
+        assert_eq!(to_cell(File::FileF, Rank::Rank3), Cell::F3);
+        assert_eq!(to_cell(File::FileF, Rank::Rank4), Cell::F4);
+        assert_eq!(to_cell(File::FileF, Rank::Rank5), Cell::F5);
+        assert_eq!(to_cell(File::FileF, Rank::Rank6), Cell::F6);
+        assert_eq!(to_cell(File::FileF, Rank::Rank7), Cell::F7);
+        assert_eq!(to_cell(File::FileF, Rank::Rank8), Cell::F8);
+        assert_eq!(to_cell(File::FileG, Rank::Rank1), Cell::G1);
+        assert_eq!(to_cell(File::FileG, Rank::Rank2), Cell::G2);
+        assert_eq!(to_cell(File::FileG, Rank::Rank3), Cell::G3);
+        assert_eq!(to_cell(File::FileG, Rank::Rank4), Cell::G4);
+        assert_eq!(to_cell(File::FileG, Rank::Rank5), Cell::G5);
+        assert_eq!(to_cell(File::FileG, Rank::Rank6), Cell::G6);
+        assert_eq!(to_cell(File::FileG, Rank::Rank7), Cell::G7);
+        assert_eq!(to_cell(File::FileG, Rank::Rank8), Cell::G8);
+        assert_eq!(to_cell(File::FileH, Rank::Rank1), Cell::H1);
+        assert_eq!(to_cell(File::FileH, Rank::Rank2), Cell::H2);
+        assert_eq!(to_cell(File::FileH, Rank::Rank3), Cell::H3);
+        assert_eq!(to_cell(File::FileH, Rank::Rank4), Cell::H4);
+        assert_eq!(to_cell(File::FileH, Rank::Rank5), Cell::H5);
+        assert_eq!(to_cell(File::FileH, Rank::Rank6), Cell::H6);
+        assert_eq!(to_cell(File::FileH, Rank::Rank7), Cell::H7);
+        assert_eq!(to_cell(File::FileH, Rank::Rank8), Cell::H8);
+    }
+}
